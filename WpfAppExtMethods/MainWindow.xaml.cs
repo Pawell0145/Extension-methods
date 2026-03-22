5fe09c0dd175583cbfb54deb89ac003befde92ba
Paweł <Pawe@DESKTOP-4P5O6RJ>
2026-03-22 13:23:13 +0100

    added GUI in XAML

---
+++ WpfAppExtMethods/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace ExtensionMethodsTester
+{
+    public partial class MainWindow : Window
+    {
+        StringManipulator _manipulator = new StringManipulator();
+
+        private void BtnCapitalize_Click(object sender, RoutedEventArgs e)
+        {
+            OutputTextBox.Text = _manipulator.CapitalizeLetter(InputTextBox.Text);
+        }
+
+        private void BtnRemoveVowels_Click(object sender, RoutedEventArgs e)
+        {
+            OutputTextBox.Text = _manipulator.RemoveVowels(InputTextBox.Text);
+        }
+
+        private void BtnLengthOfWords_Click(object sender, RoutedEventArgs e)
+        {
+            OutputTextBox.Text = _manipulator.LengthOfWords(InputTextBox.Text);
+        }
+
+        private void BtnIsProperSentence_Click(object sender, RoutedEventArgs e)
+        {
+            bool isProper = _manipulator.IsProperSentence(InputTextBox.Text);
+            OutputTextBox.Text = isProper ? "True - It is a proper sentence." : "False - It is NOT a proper sentence.";
+        }
+    }
+}
---
+++ WpfAppExtMethods/StringManipulator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+public class StringManipulator
+{
+    public StringManipulator()
+    { }
+        public string CapitalizeLetter(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            char[] chars = input.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (i % 2 == 0)
+                    chars[i] = char.ToUpper(chars[i]);
+                else
+                    chars[i] = char.ToLower(chars[i]);
+            }
+
+            return new string(chars);
+        }
+
+        public string RemoveVowels(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            var chars = input
+                .Where(c => c != ' ' && !"aeiouAEIOU.".Contains(c))
+                .ToArray();
+
+            var result = string.Join(" ", chars);
+
+            return result;
+        }
+
+        public string LengthOfWords(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            var words = input.Split(' ');
+            var lengths = words.Select(w => w.Length);
+            var result = string.Join(" ", lengths);
+
+            return result;
+        }
+
+        public bool IsProperSentence(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            return char.IsUpper(input[0]) && input.EndsWith(".");
+        }
+    
+}
