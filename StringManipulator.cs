f159745e65eee11f4b26fa9545ba46e5022487f8
Paweł <Pawe@DESKTOP-4P5O6RJ>
2026-03-22 11:32:42 +0100

    added method that capitalizes every second letter and lowers every other

---
+++ StringManipulator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class StringManipulator
+{
+	public StringManipulator()
+	{
+        private string CapitalizeLetter(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            char[] chars = input.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (i % 2 == 0)
+                    chars[i] = char.ToUpper(chars[i]);
+                else
+                    chars[i] = char.ToLower(chars[i]);
+            }
+
+            return new string(chars);
+        }
+
+
+
+    }
+}
